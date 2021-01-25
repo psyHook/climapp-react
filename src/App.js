91ff49723eb76ff9b65d2ec844ea96c862e3b35c
Juan Galarce <j6alarce@gmail.com>
2021-01-25 11:46:33 -0300

    Se crea el componente del formulario y se almacenan los datos en el state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Fragment } from 'react'
-import Header from './components/Header';
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
     <Fragment>
-      <Header
-        titulo='Climapp React'
-      />
+      <Header titulo="Climapp React" />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario />
+            </div>
+            <div className="col m6 s12">2</div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
